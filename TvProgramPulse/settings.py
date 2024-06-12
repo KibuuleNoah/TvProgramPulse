bdca9c1dd5561986ffccff7dc9d54ade90faea0d
KibuuleNoah <noahkibuule3@gmail.com>
2024-06-12 17:08:10 +0300

    init demo

--- TvProgramPulse/settings.py
+++ TvProgramPulse/settings.py
@@ -77,14 +77,11 @@ WSGI_APPLICATION = "TvProgramPulse.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "TvProgramPulse",
-        "USER": "root",
-        "PASSWORD": "noahtri",
-        "HOST": "localhost",
-        "PORT": "8001",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
