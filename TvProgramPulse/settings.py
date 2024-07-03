2ac53398cc8510137137c3e409f3c29871faede4
KibuuleNoah <noahkibuule3@gmail.com>
2024-07-03 09:33:00 +0300

    resolve static issues

--- TvProgramPulse/settings.py
+++ TvProgramPulse/settings.py
@@ -52,7 +52,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
 
 ROOT_URLCONF = "TvProgramPulse.urls"
 
@@ -120,14 +122,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+import os
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = "/static/"
 
-STATIC_ROOT = "staticfiles"
-# os.path.join(os.path.dirname(BASE_DIR), "users/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+# STATIC_URL = "static/"
+#
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+#
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
